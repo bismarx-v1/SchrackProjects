d7c4167b6ca6b7207aca2ce1efb2f2f1448d1968
Natani Simak <matej.simacek.2@gmail.com>
2024-08-20 21:48:28 +0200

    a small cleanup
    moved a few things in docs around
    deleted old backups
    
    \SchrackPanel\Docs\MenusOfThePanel\
    - older version of \SchrackPanel\Docs\!MenusOfThePanel\
    \SchrackPanel\Code\Jonas'sTests\
    - now useless. deleted
    \SchrackPanel\Code\PinInterruptTest\
    - now useless. deleted
    \SchrackPanel\Code\V2\
    - very useless. deleted
    \SchrackPanel\Code\ESP Wroom 32\LedDisplayDriver\
    - now useless. deleted
    \SchrackPanel\Code\ESP Wroom 32\RandomSrTest\
    - now useless. deleted
    \SchrackPanel\Code\ESP Wroom 32\RandomTest\
    - some now useless tests. deleted
    \SchrackPanel\Code\ESP Wroom 32\ShitfuckBullshit\
    - lot of now useless tests. deleted
    \SchrackPanel\Code\ESP Wroom 32\
    - rest of this dir was more of the same. now useless. deleted

--- SchrackPanel/Code/V2/FixFilepathError.py
+++
@@ -1,9 +0,0 @@
-import os
-import re
-
-path = re.sub("/V2.*", "", re.sub("\\\\", "/", str(os.path.abspath(os.getcwd()))))
-f = open("LibFilepathFix.h", "w")
-f.write("#include \"" + str(path) + "/V2/Libs/PinDefs.h\"\n")
-f.write("#include \"" + str(path) + "/V2/Libs/Driver.h\"\n")
-f.write("#include \"" + str(path) + "/V2/Libs/ToFrame.h\"")
-f.close()
--- SchrackPanel/Code/V2/LibFilepathFix.h
+++
@@ -1,3 +0,0 @@
-#include "C:/Users/matej/Documents/GitHub/Schrack_stul/Code/V2/Libs/PinDefs.h"
-#include "C:/Users/matej/Documents/GitHub/Schrack_stul/Code/V2/Libs/Driver.h"
-#include "C:/Users/matej/Documents/GitHub/Schrack_stul/Code/V2/Libs/ToFrame.h"
