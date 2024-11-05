bfccfc368aee590d95f654a5adf6ba2d32b7a3b4
Natani Simak <matej.simacek.2@gmail.com>
2024-11-05 18:44:18 +0100

    small updates

--- TEMP_Nats/spiSender/src/main.cpp
+++ TEMP_Nats/spiSender/src/main.cpp
@@ -1,30 +1,30 @@
 #include <Arduino.h>
 
 #define MOSI 7
-#define CLK	 6
-#define CS	 5
+#define CLK  6
+#define CS   5
 
 void setup() {
-	pinMode(MOSI, OUTPUT);
-	pinMode(CLK, OUTPUT);
-	pinMode(CS, OUTPUT);
-	digitalWrite(CS, 1);
+  pinMode(MOSI, OUTPUT);
+  pinMode(CLK, OUTPUT);
+  pinMode(CS, OUTPUT);
+  digitalWrite(CS, 1);
 
-	Serial.begin(115200);
+  Serial.begin(115200);
 }
 
 void loop() {
-	if(Serial.available() > 1) {
-		uint8_t reg = Serial.read();
-		uint8_t val = Serial.read();
+  if(Serial.available() > 1) {
+    uint8_t reg = Serial.read();
+    uint8_t val = Serial.read();
 
-		Serial.print(reg, HEX);
-		Serial.print("\t");
-		Serial.println(val, HEX);
+    Serial.print(reg, HEX);
+    Serial.print("\t");
+    Serial.println(val, HEX);
 
-		digitalWrite(CS, 0);
-		shiftOut(MOSI, CLK, MSBFIRST, reg);
-		shiftOut(MOSI, CLK, MSBFIRST, val);
-		digitalWrite(CS, 1);
-	}
+    digitalWrite(CS, 0);
+    shiftOut(MOSI, CLK, MSBFIRST, reg);
+    shiftOut(MOSI, CLK, MSBFIRST, val);
+    digitalWrite(CS, 1);
+  }
 }
